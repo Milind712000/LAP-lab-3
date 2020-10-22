f02496cf1ad5c8004ce0cd18eed38bfdf013a625
snowsanta <45097716+snowsanta@users.noreply.github.com>
2020-10-22 18:25:20 +0530

    added stats support

--- main.py
+++ main.py
@@ -2,67 +2,74 @@ import PySimpleGUI as sg
 import os
 from statHelper import getStats, extractSentences, plotFrequency
 
+
 def getTxt(fpath):
-	if(fpath == ''):
-		# do checks to see if file path is valid
-		sg.popup_annoying('Given file path is invalid')
-		return ''
-	ltxt = ''
-	with open(fpath) as fh:
-		ltxt = fh.read()
-	return ltxt
+    if(fpath == ''):
+        # do checks to see if file path is valid
+        sg.popup_annoying('Given file path is invalid')
+        return ''
+    ltxt = ''
+    with open(fpath, "r", encoding="utf-8") as fh:
+        ltxt = fh.read()
+    return ltxt
+
+
+def getAvoidDict(fpath):
+    avoidWords = set(open(fpath, "r", encoding="utf-8").read().split())
+    return avoidWords
+
 
 # filepicker 1
 fp1 = [
-	sg.Text("Main File"),
-	sg.In(key="-mainFile-"),
-	sg.FileBrowse(target="-mainFile-"),
-	sg.Button("Analyse")
+    sg.Text("Main File"),
+    sg.In(key="-mainFile-"),
+    sg.FileBrowse(target="-mainFile-"),
+    sg.Button("Analyse")
 ]
 
 # filepicker 2
 fp2 = [
-	sg.Text("Keywords File"),
-	sg.In(key="-keyFile-"),
-	sg.FileBrowse(target="-keyFile-"),
-	sg.Button("Extract")
+    sg.Text("Keywords File"),
+    sg.In(key="-keyFile-"),
+    sg.FileBrowse(target="-keyFile-"),
+    sg.Button("Extract")
 ]
 
 statBox = [
-	sg.Text("Statistics"),
-	sg.Multiline(key='-bigblob-', size=(100, 5))
+    sg.Text("Statistics"),
+    sg.Multiline(key='-bigblob-', size=(100, 5))
 ]
 
 grepBox = [
-	sg.Text("Extracted Sentences"),
-	sg.Multiline(key='-grep-', size=(100, 5))
+    sg.Text("Extracted Sentences"),
+    sg.Multiline(key='-grep-', size=(100, 5))
 ]
 
 topControls = [
-	sg.Button('Refresh'), 
-	sg.Button('Reset'), 
-	sg.Exit()
+    sg.Button('Refresh'),
+    sg.Button('Reset'),
+    sg.Exit()
 ]
 
 histoPlot = [
-	[sg.Text("Histogram")],
-	[sg.Image(key="-PLOT-")]
+    [sg.Text("Histogram")],
+    [sg.Image(key="-PLOT-")]
 ]
 
 leftCol = [
-	fp1,
-	fp2,
-	statBox,
-	grepBox
+    fp1,
+    fp2,
+    statBox,
+    grepBox
 ]
 
 layout = [
-	topControls,
-	[
-		sg.Column(leftCol),
-		sg.VSeperator(),
-		sg.Column(histoPlot)
-	]
+    topControls,
+    [
+        sg.Column(leftCol),
+        sg.VSeperator(),
+        sg.Column(histoPlot)
+    ]
 ]
 
 
@@ -70,6 +77,7 @@ baseDir = os.path.dirname(os.path.realpath(__file__))
 # DATA
 # txt from input file
 ltxt = ''
+avoidWords = ''
 keyText = ''
 stats = ''
 extSnts = ''
@@ -80,36 +88,38 @@ plotPath = blankFig
 window = sg.Window('Text Analyze', layout, finalize=True)
 window['-PLOT-'].Update(plotPath)
 
-while True: # The Event Loop
-	event, values = window.read() 
-	if event == sg.WIN_CLOSED or event == 'Exit':
-		break
+while True:  # The Event Loop
+    event, values = window.read()
+    if event == sg.WIN_CLOSED or event == 'Exit':
+        break
 
-	if event == 'Analyse' or event == 'Refresh':
-		# update txt
-		ltxt = getTxt(values['-mainFile-'])
-		# update stats
-		stats = getStats(ltxt)
-		# update plot
-		plotFrequency(ltxt, histoFig)
-		plotPath = histoFig
-	
-	if event == 'Reset':
-		ltxt = ''
-		keyText = ''
-		stats = ''
-		extSnts = ''
-		window['-mainFile-'].Update('')
-		window['-keyFile-'].Update('')
-		plotPath = blankFig
+    if event == 'Analyse' or event == 'Refresh':
+        # update txt
+        ltxt = getTxt(values['-mainFile-'])
+        # update avoid list
+        avoidWords = getAvoidDict("words_to_avoid.txt")
+        # update stats
+        stats = getStats(ltxt, avoidWords)
+        # update plot
+        plotFrequency(ltxt, histoFig)
+        plotPath = histoFig
 
-	if event == 'Extract' or event == 'Refresh':
-		keyText = getTxt(values['-keyFile-'])
-		extSnts = extractSentences(ltxt, keyText)
+    if event == 'Reset':
+        ltxt = ''
+        keyText = ''
+        stats = ''
+        extSnts = ''
+        window['-mainFile-'].Update('')
+        window['-keyFile-'].Update('')
+        plotPath = blankFig
 
-	window['-bigblob-'].Update(stats)
-	window['-grep-'].Update(extSnts)
-	window['-PLOT-'].Update(plotPath)
-	
+    if event == 'Extract' or event == 'Refresh':
+        keyText = getTxt(values['-keyFile-'])
+        extSnts = extractSentences(ltxt, keyText)
+
+    window['-bigblob-'].Update(stats)
+    window['-grep-'].Update(extSnts)
+    window['-PLOT-'].Update(plotPath)
+
 
 window.close()
--- statHelper.py
+++ statHelper.py
@@ -1,12 +1,40 @@
-def getStats(txt):
+from collections import Counter
+
+
+def getStats(txt, avoid_list):
     # return string of answer
-    return txt
+    lines = txt.count("\n")
+    sentences = txt.count(".")
+    mostf = ""
+    leastf = ""
+
+    wordcount = Counter(txt.replace(".", " ").replace("\n", " ").replace(
+        ",", " ").replace(";", " ").replace("_", " ").lower().split())
+
+    sorted_words = wordcount.most_common()
+    # most freq
+    for x in range(len(sorted_words)):
+        if(sorted_words[x][0] not in avoid_list):
+            mostf = sorted_words[x][0]
+            break
+
+    # # least freq
+    for x in range(len(sorted_words) - 1, -1, -1):
+        if(sorted_words[x][0] not in avoid_list):
+            leastf = sorted_words[x][0]
+            break
+
+    stats = "Number of words in file = " + str(sum(wordcount.values())) + "\nNumber of sentences in file = " + str(
+        sentences) + "\nNumber of newlines in file = " + str(lines) + "\nMost frequent word in file = " + str(mostf) + "\nLeast frequent word in file = " + str(leastf)
+
+    return stats
+
 
 def extractSentences(txt, keyTxt):
     # return string of answer
     return keyTxt
 
+
 def plotFrequency(txt, fpath):
     # plot histogram and store at given fpath as png
-    x = 0
     return
